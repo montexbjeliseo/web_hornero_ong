ffc5a3dd62019d7fe9314938839ceb0a24ff76d8
montexbjeliseo <secondmtx@gmail.com>
2023-11-16 16:15:46 -0300

    Some minor fixes

--- apps/contacts/signals.py
+++ apps/contacts/signals.py
@@ -1,9 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
-from django.contrib.auth.models import User
-from django.contrib.sites.shortcuts import get_current_site
-from django.http import HttpRequest
 from django.conf import settings
 
 from . import models
@@ -11,8 +8,8 @@ from . import models
 @receiver(post_save, sender=models.Contact)
 def enviar_mensaje_nuevo_contacto(sender, instance, **kwargs):
     
-    asunto = 'Nuevo recurso creado'
+    asunto = 'Nuevo contacto creado'
     mensaje = f'{instance.name} te ha dejado un mensaje en el sitio\nVer en el sitio: http://{settings.DOMAIN}{instance.get_absolute_url()}'
-    destinatario = ['secondmtx@gmail.com'] 
+    destinatario = [settings.EMAIL_HOST_USER] 
     
     send_mail(asunto, mensaje, None, destinatario, fail_silently=False)
